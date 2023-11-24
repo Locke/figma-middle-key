49d5eb9881a6c9207ac3efeaab3663e9bcdc5c78
André Wolski <locke@dena-design.de>
2023-11-24 18:00:46 +0100

    adapt to tampermonkey

--- figma-middle-key.user.js
+++ figma-middle-key.user.js
@@ -1,42 +1,48 @@
 // ==UserScript==
 // @name         figma-middle-key
-// @version      1
+// @namespace    http://tampermonkey.net/
+// @version      2
 // @description  Disables pasting with the middle mouse button on the canvas
-// @author       Yeahright
+// @author       Yeahright, André Wolski
 // @match        https://www.figma.com/file/*
 // @grant        none
 // ==/UserScript==
-const middle_button = 1
 
-let nopaste = false
+(function() {
+    'use strict';
 
-const delta = 6;
-let startX;
-let startY;
+    const middle_button = 1
 
-document.addEventListener('mousedown', function (event) {
-  startX = event.pageX;
-  startY = event.pageY;
-});
+    let nopaste = false
 
-document.addEventListener('mouseup', function(e) {
-    const diffX = Math.abs(e.pageX - startX);
-    const diffY = Math.abs(e.pageY - startY);
-  	if (diffX < delta && diffY < delta) {
-      // It's a click, not a drag
-      return false 
-  	}
-    if (e.button == middle_button) {
-      	nopaste = true
-    }
-    return false;
-}, true);
+    const delta = 6;
+    let startX;
+    let startY;
 
-document.addEventListener('paste', function(e) {
-    if (nopaste) {
-        e.cancelBubble = true;
-        e.stopImmediatePropagation();
-      	nopaste = false
-    }
-    return false;
-}, true);
+    document.addEventListener('mousedown', function (event) {
+        startX = event.pageX;
+        startY = event.pageY;
+    });
+
+    document.addEventListener('mouseup', function(e) {
+        const diffX = Math.abs(e.pageX - startX);
+        const diffY = Math.abs(e.pageY - startY);
+        if (diffX < delta && diffY < delta) {
+            // It's a click, not a drag
+            return false;
+        }
+        if (e.button == middle_button) {
+            nopaste = true
+        }
+        return false;
+    }, true);
+
+    document.addEventListener('paste', function(e) {
+        if (nopaste) {
+            e.cancelBubble = true;
+            e.stopImmediatePropagation();
+            nopaste = false
+        }
+        return false;
+    }, true);
+})();
